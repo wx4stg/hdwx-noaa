c0eb83894db5e4e74df6bcf6606ff1efd4f249bd
Sam Gardner <sam@wx4stg.com>
2023-06-15 05:41:14 +0000

    add station plots and contoured MSLP

--- surfacebulletin.py
+++ surfacebulletin.py
@@ -4,9 +4,20 @@ import matplotlib.pyplot as plt
 import urllib.request
 from io import BytesIO
 from metpy.io import parse_wpc_surface_bulletin
+from metpy.io import parse_metar_file
+from metpy.units import pandas_dataframe_to_unit_arrays
+from metpy import calc as mpcalc
+from metpy import plots as mpplots
+from metpy.units import units
 from metpy.plots import ColdFront, OccludedFront, StationaryFront, StationPlot, WarmFront
-from os import path, listdir
+from metpy.cbook import get_test_data
+from os import path, listdir, remove
 from pathlib import Path
+from siphon.catalog import TDSCatalog
+import pandas as pd
+from matplotlib.patheffects import withStroke
+import numpy as np
+from scipy.interpolate import griddata
 
 
 basePath = path.realpath(path.dirname(__file__))
@@ -25,6 +36,47 @@ def set_size(w,h, ax=None):
     figw = float(w)/(r-l)
     figh = float(h)/(t-b)
     ax.figure.set_size_inches(figw, figh)
+
+
+
+def addStationPlot(ax, validTime):
+    metarTime = validTime.replace(minute=0, second=0, microsecond=0)
+    stationCatalog = TDSCatalog("https://thredds.ucar.edu/thredds/catalog/noaaport/text/metar/catalog.xml")
+    airports = pd.read_csv(get_test_data("airport-codes.csv"))
+    airports = airports[(airports["type"] == "large_airport") | (airports["type"] == "medium_airport") | (airports["type"] == "small_airport")]
+    try:
+        dataset = stationCatalog.datasets.filter_time_nearest(metarTime)
+        dataset.download()
+        [remove(file) for file in sorted(listdir()) if "metar_" in file and file != dataset.name]
+    except Exception as e:
+        print(stationCatalog.datasets.filter_time_nearest(metarTime).remote_open().read())
+    if path.exists(dataset.name):
+        metarData = parse_metar_file(dataset.name, year=metarTime.year, month=metarTime.month)
+    else:
+        return
+    metarUnits = metarData.units
+
+    metarDataFilt = metarData[metarData["station_id"].isin(airports["ident"])]
+    metarDataFilt = metarDataFilt.dropna(how="any", subset=["longitude", "latitude", "station_id", "wind_speed", "wind_direction", "air_temperature", "dew_point_temperature", "air_pressure_at_sea_level", "current_wx1_symbol", "cloud_coverage"])
+    metarDataFilt = metarDataFilt.drop_duplicates(subset=["station_id"], keep="last")
+    metarData = pandas_dataframe_to_unit_arrays(metarDataFilt, metarUnits)
+    metarData["u"], metarData["v"] = mpcalc.wind_components(metarData["wind_speed"], metarData["wind_direction"])
+    lonGrid = np.linspace(-130, -60, 1000)
+    latGrid = np.linspace(20, 50, 1000)
+    lonGrid, latGrid = np.meshgrid(lonGrid, latGrid)
+    mslpGrid = griddata((metarData["longitude"], metarData["latitude"]), metarData["air_pressure_at_sea_level"], (lonGrid, latGrid), method="linear")
+    ax.contour(lonGrid, latGrid, mslpGrid, colors="maroon", transform=ccrs.PlateCarree(), linewidths=1, levels=range(900, 1100, 4))
+    locationsInMeters = ccrs.LambertConformal().transform_points(ccrs.PlateCarree(), metarData["longitude"].m, metarData["latitude"].m)
+    overlap_prevent = mpcalc.reduce_point_density(locationsInMeters[:, 0:2], 200000)
+    stations = mpplots.StationPlot(ax, metarData["longitude"][overlap_prevent], metarData["latitude"][overlap_prevent], clip_on=True, transform=ccrs.PlateCarree(), fontsize=6)
+    stations.plot_parameter("NW", metarData["air_temperature"][overlap_prevent].to(units.degF), path_effects=[withStroke(linewidth=1, foreground="white")])
+    stations.plot_parameter("SW", metarData["dew_point_temperature"][overlap_prevent].to(units.degF), path_effects=[withStroke(linewidth=1, foreground="white")])
+    stations.plot_parameter("NE", metarData["air_pressure_at_sea_level"][overlap_prevent].to(units.hPa), formatter=lambda v: format(10 * v, '.0f')[-3:], path_effects=[withStroke(linewidth=1, foreground="white")])
+    stations.plot_symbol((-1.5, 0), metarData['current_wx1_symbol'][overlap_prevent], mpplots.current_weather, path_effects=[withStroke(linewidth=1, foreground="white")], fontsize=9)
+    stations.plot_symbol("C", metarData["cloud_coverage"][overlap_prevent], mpplots.sky_cover)
+    stations.plot_barb(metarData["u"][overlap_prevent], metarData["v"][overlap_prevent], sizes={"emptybarb" : 0})
+    return ax
+
 
 def plot_bulletin(ax, data):
     """Plot a dataframe of surface features on a map."""
@@ -78,6 +130,7 @@ if __name__ == "__main__":
     ax.set_extent([-130, -60, 20, 50], crs=ccrs.PlateCarree())
  
     plot_bulletin(gisAx, df)
+    addStationPlot(ax, validTime)
     plot_bulletin(ax, df)
 
     px = 1/plt.rcParams["figure.dpi"]
