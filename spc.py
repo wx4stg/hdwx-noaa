1a017e3eac2607ebdca1c5d0d3d45b50dccb4f27
Sam Gardner <sam@wx4stg.com>
2023-06-23 10:09:36 +0000

    add spc fire outlooks

--- spc.py
+++ spc.py
@@ -45,6 +45,10 @@ def plotOutlook(data, dayNum, startTime, finalTime, issueTime, outlookType):
         legend_elements.append(Patch(facecolor="#00000000", edgecolor="#00000000", label="Chance of any severe weather within 25 miles of a point"))
         prettyOutlookType = "Probabilistic"
         sigRisk = "(10% chance of 2\" or larger hail, or EF2 or stronger tornado, or 74 mph or stronger wind)"
+    elif outlookType == "fire":
+        legend_elements.append(Patch(facecolor="#00000000", edgecolor="#00000000", label="Classification Info:\nhttps://www.spc.noaa.gov/misc/about.html#FireWx"))
+        prettyOutlookType = "Fire Weather"
+        sigRisk = ""
     fig = plt.figure()
     px = 1/plt.rcParams["figure.dpi"]
     fig.set_size_inches(1920*px, 1080*px)
@@ -58,7 +62,7 @@ def plotOutlook(data, dayNum, startTime, finalTime, issueTime, outlookType):
             continue
         fcolor = str(polysForCat["fill"].iloc[0])
         ecolor = str(polysForCat["stroke"].iloc[0])
-        label = str(polysForCat["LABEL2"].iloc[0]).replace("General ", "").replace("s Risk", "").replace(" Hail", "").replace(" Tornado", "").replace(" Wind", "").replace(" Risk", "")
+        label = str(polysForCat["LABEL2"].iloc[0]).replace("General ", "").replace("s Risk", "").replace(" Hail", "").replace(" Tornado", "").replace(" Wind", "").replace(" Fire", "").replace(" Risk", "")
         if "Significant" in label:
             ax.add_geometries(polysForCat["geometry"], crs=ccrs.PlateCarree(), facecolor="#00000000", edgecolor=ecolor, linewidth=1, hatch="///")
             legend_elements.append(Patch(facecolor="#00000000", edgecolor=ecolor, hatch="///", label=f"{label} {sigRisk}"))
@@ -89,6 +93,8 @@ def plotOutlook(data, dayNum, startTime, finalTime, issueTime, outlookType):
         savePaths["1211"] = path.join(basePath, "output", "products", "noaa", "spc", "windout")
     elif outlookType == "torn":
         savePaths["1213"] = path.join(basePath, "output", "products", "noaa", "spc", "tornout")
+    elif outlookType == "fire":
+        savePaths["1215"] = path.join(basePath, "output", "products", "noaa", "spc", "fireout")
     for productID in savePaths.keys():
         savePath = savePaths[productID]
         productID = int(productID)
@@ -99,6 +105,7 @@ def plotOutlook(data, dayNum, startTime, finalTime, issueTime, outlookType):
             HDWX_helpers.saveImage(fig, path.join(savePath, f"day{dayNum}.png"))
         else:
             fig.savefig(path.join(savePath, f"day{dayNum}.png"))
+    plt.close(fig)
 
 
 
@@ -137,5 +144,31 @@ if __name__ == "__main__":
                 continue
             gpdata = geopandas.read_file(path.join(convDayInputPath, shapeFile))
             plotOutlook(gpdata, dayNum, startTime, endTime, issueTime, outlookType)
+    for dayNum in range(1, 9):
+        convDayInputPath = path.join(inputPath, f"day{dayNum}-fire")
+        Path(convDayInputPath).mkdir(parents=True, exist_ok=True)
+        if dayNum < 3:
+            file = requests.get(f"https://www.spc.noaa.gov/products/fire_wx/day{dayNum}firewx-shp.zip")
+        else:
+            file = requests.get(f"https://www.spc.noaa.gov/products/exper/fire_wx/day{dayNum}firewx-shp.zip")
+        z = zipfile.ZipFile(BytesIO(file.content))
+        z.extractall(convDayInputPath)
+        filesInTarget = listdir(convDayInputPath)
+        infoFile = None
+        shapeFiles = []
+        for file in filesInTarget:
+            if file.endswith(".info"):
+                infoFile = file
+            elif file.endswith(".shp"):
+                shapeFiles.append(file)
+        with open(path.join(convDayInputPath, infoFile), "r") as infoFileHandle:
+            infoLines = infoFileHandle.readlines()
+        startTime = dt.strptime(infoLines[0], "Product Valid Time Begin: %Y-%m-%d %H:%M:%S+00:00\n")
+        endTime =  dt.strptime(infoLines[1], "Product Valid Time End: %Y-%m-%d %H:%M:%S+00:00\n")
+        for shapeFile in shapeFiles:
+            if "dryltg" in shapeFile:
+                continue
+            gpdata = geopandas.read_file(path.join(convDayInputPath, shapeFile))
+            plotOutlook(gpdata, dayNum, startTime, endTime, issueTime, "fire")
     rmtree(path.join(basePath, "input"))
     
